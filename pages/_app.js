d27763bad88639a5fd67d27db0b9abdcbe457005
Gaurav Achhra <achhragaurav@gmail.com>
2022-05-13 21:29:52 +0530

    First

--- pages/_app.js
+++ pages/_app.js
@@ -4,10 +4,38 @@ import { useEffect } from 'react'
 import { AnimatePresence } from 'framer-motion'
 
 
+
 import '../styles/globals.css'
 
 
-function MyApp({ Component, pageProps, router }){
+function MyApp({ Component, pageProps, router }) {
+    
+
+useEffect(() => {
+    Array.from(
+        document.querySelectorAll('head > link[rel="stylesheet"][data-n-p]')
+    ).forEach(node => {
+        node.removeAttribute('data-n-p');
+    });
+    const mutationHandler = mutations => {
+        mutations.forEach(({ target }) => {
+            if (target.nodeName === 'STYLE') {
+                if (target.getAttribute('media') === 'x') {
+                    target.removeAttribute('media');
+                }
+            }
+        });
+    };
+    const observer = new MutationObserver(mutationHandler);
+    observer.observe(document.head, {
+        subtree: true,
+        attributeFilter: ['media'],
+    });
+    return () => {
+        observer.disconnect();
+    };
+}, []);
+
     const url = `https://wallis.dev${router.route}`
 
     useEffect(() => {
